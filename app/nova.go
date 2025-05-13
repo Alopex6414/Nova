d23508852f610b2a9babafa786e33936d6e26984
Alopex6414 <1029535012@qq.com>
2025-05-13 23:20:54 +0800

    create user management related functions and add routes

--- app/nova.go
+++ app/nova.go
@@ -26,6 +26,11 @@ func (nova *Nova) Start() {
 	novaService := router.Group("nova/v1")
 	{
 		novaService.GET("/test", func(c *gin.Context) { c.String(http.StatusOK, "hello Gin\n") })
+		// user management
+		novaService.POST("/user/:userId", handleCreatUser)
+		novaService.DELETE("/user/:userId", handleDeleteUser)
+		novaService.PATCH("/user/:userId", handleModifyUser)
+		novaService.GET("/user/:userId", handleQueryUser)
 	}
 	// start http service
 	server := &http.Server{
--- app/service.go
+++ app/service.go
@@ -1,1 +1,19 @@
 package app
+
+import "github.com/gin-gonic/gin"
+
+func handleCreatUser(c *gin.Context) {
+	return
+}
+
+func handleDeleteUser(c *gin.Context) {
+	return
+}
+
+func handleModifyUser(c *gin.Context) {
+	return
+}
+
+func handleQueryUser(c *gin.Context) {
+	return
+}
