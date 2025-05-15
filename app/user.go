53603976123f641106a36fbfa6322a878d8161e4
Alopex6414 <1029535012@qq.com>
2025-05-15 22:44:41 +0800

    add handleCreateUser basic function

--- app/user.go
+++ app/user.go
@@ -1,8 +1,21 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func handleCreatUser(c *gin.Context) {
+	var user User
+	// request body should bind json
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	// return response
+	c.Header("Content-Type", "application/json")
+	c.JSON(http.StatusCreated, user)
 	return
 }
 
