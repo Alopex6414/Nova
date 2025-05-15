0d8cee723c13efa7e8f1fe49e578a38a44c87519
Alopex6414 <1029535012@qq.com>
2025-05-15 22:16:34 +0800

    create types.go and define User type

---
+++ app/types.go
@@ -0,0 +1,11 @@
+package app
+
+type User struct {
+	UserId      string `json:"userId" yaml:"userId" binding:"required"`
+	Username    string `json:"username" yaml:"username" binding:"required"`
+	Password    string `json:"password" yaml:"password" binding:"required"`
+	PhoneNumber string `json:"phone_number" yaml:"phone_number" binding:"required"`
+	Email       string `json:"email" yaml:"email" binding:"required"`
+	Address     string `json:"address" yaml:"address" binding:"required"`
+	Company     string `json:"company" yaml:"company" binding:"required"`
+}
