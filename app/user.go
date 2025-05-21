50d848ca46ab33a970586bd73fa268c2eb90b504
Alopex6414 <1029535012@qq.com>
2025-05-21 22:12:19 +0800

    implement user delete

--- app/user.go
+++ app/user.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"net/http"
+	"strings"
 )
 
 func (nova *Nova) HandleCreateUserId(c *gin.Context) {
@@ -48,8 +49,8 @@ func (nova *Nova) HandleCreateUser(c *gin.Context) {
 		return
 	}
 	// store created user in data cache
-	user := User{
-		UserId:      request.UserId,
+	response := User{
+		UserId:      strings.ToLower(request.UserId),
 		Username:    request.Username,
 		Password:    request.Password,
 		PhoneNumber: request.PhoneNumber,
@@ -63,15 +64,55 @@ func (nova *Nova) HandleCreateUser(c *gin.Context) {
 		defer nova.cache.userCache.mutex.Unlock()
 		// append user in data cache
 		nova.cache.userCache.userSet = append(nova.cache.userCache.userSet, user)
-	}(user)
+	}(response)
 	// return response
-	var response = request
 	c.Header("Content-Type", "application/json")
 	c.JSON(http.StatusCreated, response)
 	return
 }
 
 func (nova *Nova) HandleDeleteUser(c *gin.Context) {
+	// extract userId from uri
+	userId := strings.ToLower(c.Param("userId"))
+	// check user existence
+	b := func(userId string) bool {
+		// enable user cache read lock
+		nova.cache.userCache.mutex.RLock()
+		defer nova.cache.userCache.mutex.RUnlock()
+		// search userId in data cache
+		for _, v := range nova.cache.userCache.userSet {
+			if v.UserId == userId {
+				return true
+			}
+		}
+		return false
+	}(userId)
+	if b {
+		var problemDetails ProblemDetails
+		problemDetails.Title = "User Not Found"
+		problemDetails.Type = "User"
+		problemDetails.Status = http.StatusNotFound
+		problemDetails.Cause = errors.New("user not found").Error()
+		c.Header("Content-Type", "application/problem+json")
+		c.JSON(http.StatusNotFound, problemDetails)
+		return
+	}
+	// delete user from data cache
+	func(userId string) {
+		// enable user cache write lock
+		nova.cache.userCache.mutex.Lock()
+		defer nova.cache.userCache.mutex.Unlock()
+		// search & delete user from data cache
+		for k, v := range nova.cache.userCache.userSet {
+			if v.UserId == userId {
+				nova.cache.userCache.userSet = append(nova.cache.userCache.userSet[:k], nova.cache.userCache.userSet[k+1:]...)
+				break
+			}
+		}
+		return
+	}(userId)
+	// return response
+	c.Status(http.StatusNoContent)
 	return
 }
 
