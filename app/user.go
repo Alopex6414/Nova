83ab18fce0a1f70c88f996b674d0983f6d096e4f
Alopex6414 <1029535012@qq.com>
2025-05-22 21:49:51 +0800

    implement HandleReplaceUser

--- app/user.go
+++ app/user.go
@@ -22,7 +22,13 @@ func (nova *Nova) HandleCreateUser(c *gin.Context) {
 	// request body should bind json
 	err := c.ShouldBindJSON(&request)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		var problemDetails ProblemDetails
+		problemDetails.Title = "Bad Request"
+		problemDetails.Type = "User"
+		problemDetails.Status = http.StatusBadRequest
+		problemDetails.Cause = err.Error()
+		c.Header("Content-Type", "application/problem+json")
+		c.JSON(http.StatusBadRequest, problemDetails)
 		return
 	}
 	// check request body correctness
@@ -59,7 +65,7 @@ func (nova *Nova) HandleCreateUser(c *gin.Context) {
 	}(request.UserId)
 	if b {
 		var problemDetails ProblemDetails
-		problemDetails.Title = "User Conflict"
+		problemDetails.Title = "Conflict"
 		problemDetails.Type = "User"
 		problemDetails.Status = http.StatusConflict
 		problemDetails.Cause = errors.New("user already exists").Error()
@@ -108,7 +114,7 @@ func (nova *Nova) HandleDeleteUser(c *gin.Context) {
 	}(userId)
 	if !b {
 		var problemDetails ProblemDetails
-		problemDetails.Title = "User Not Found"
+		problemDetails.Title = "Not Found"
 		problemDetails.Type = "User"
 		problemDetails.Status = http.StatusNotFound
 		problemDetails.Cause = errors.New("user not found").Error()
@@ -144,5 +150,78 @@ func (nova *Nova) HandleQueryUser(c *gin.Context) {
 }
 
 func (nova *Nova) HandleReplaceUser(c *gin.Context) {
+	var request User
+	// request body should bind json
+	err := c.ShouldBindJSON(&request)
+	if err != nil {
+		var problemDetails ProblemDetails
+		problemDetails.Title = "Bad Request"
+		problemDetails.Type = "User"
+		problemDetails.Status = http.StatusBadRequest
+		problemDetails.Cause = err.Error()
+		c.Header("Content-Type", "application/problem+json")
+		c.JSON(http.StatusBadRequest, problemDetails)
+		return
+	}
+	// check request body correctness
+	// check user existence
+	b := func(userId string) bool {
+		// enable user cache read lock
+		nova.cache.userCache.mutex.RLock()
+		defer nova.cache.userCache.mutex.RUnlock()
+		// search userId in data cache
+		for _, v := range nova.cache.userCache.userSet {
+			if v.UserId == userId {
+				return true
+			}
+		}
+		return false
+	}(request.UserId)
+	if !b {
+		var problemDetails ProblemDetails
+		problemDetails.Title = "Forbidden"
+		problemDetails.Type = "User"
+		problemDetails.Status = http.StatusForbidden
+		problemDetails.Cause = errors.New("forbidden replace user without create it").Error()
+		c.Header("Content-Type", "application/problem+json")
+		c.JSON(http.StatusForbidden, problemDetails)
+		return
+	}
+	// store created user in data cache
+	response := User{
+		UserId:      strings.ToLower(request.UserId),
+		Username:    request.Username,
+		Password:    request.Password,
+		PhoneNumber: request.PhoneNumber,
+		Email:       request.Email,
+		Address:     request.Address,
+		Company:     request.Company,
+	}
+	b = func(user User) bool {
+		// enable user cache write lock
+		nova.cache.userCache.mutex.Lock()
+		defer nova.cache.userCache.mutex.Unlock()
+		// replace user in data cache
+		for k, v := range nova.cache.userCache.userSet {
+			if v.UserId == user.UserId {
+				nova.cache.userCache.userSet[k] = user
+				return true
+			}
+		}
+		return false
+	}(response)
+	if !b {
+		var problemDetails ProblemDetails
+		problemDetails.Title = "Not Found"
+		problemDetails.Type = "User"
+		problemDetails.Status = http.StatusNotFound
+		problemDetails.Cause = errors.New("user not existed").Error()
+		c.Header("Content-Type", "application/problem+json")
+		c.JSON(http.StatusNotFound, problemDetails)
+		return
+	}
+	// return response
+	c.Header("Content-Type", "application/json")
+	c.JSON(http.StatusOK, response)
 	return
 }
