e1309fc0e311fca8ab4d5a8cc97e5a79a10e2bcc
Alopex6414 <1029535012@qq.com>
2025-06-12 21:00:34 +0800

    create mutual-tls-service.bat

---
+++ mutual-tls-service.bat
@@ -0,0 +1,7 @@
+:: Nova Project - HTTPs Service (Mutual-TLS)
+:: Copyright(C) 2019, Team Gorgeous Bubble, All Rights Reserved.
+@echo off
+
+:: Nova HTTPs Service (Mutual-TLS) - Start
+cd bin
+./nova https -ip localhost -port 8443
