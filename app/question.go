816982417bef8ead8cacd07944fab0e70bed491d
Alopex6414 <1029535012@qq.com>
2025-06-17 23:21:35 +0800

    create reflect while binding data type

--- app/question.go
+++ app/question.go
@@ -1,10 +1,13 @@
 package app
 
 import (
+	"encoding/json"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"net/http"
 	"nova/logger"
+	"reflect"
 )
 
 func (nova *Nova) HandleCreateQuestionId(c *gin.Context) {
@@ -21,7 +24,56 @@ func (nova *Nova) HandleCreateQuestionId(c *gin.Context) {
 }
 
 func (nova *Nova) HandleCreateQuestion(c *gin.Context) {
+	// create question
+	logger.Infof("handle request create question")
+	// get raw data from request body
+	logger.Debugf("get raw data from request body")
+	rawData, err := c.GetRawData()
+	if err != nil {
+		nova.response400BadRequest(c, err)
+		logger.Errorf("error get raw data from request body: %v", err)
+		return
+	}
+	logger.Debugf("successfully get raw data from request body")
+	// reflect request type
+	logger.Debugf("reflect request type")
+	reqType, err := nova.reflectRequestType(rawData)
+	if err != nil {
+		nova.response400BadRequest(c, err)
+		logger.Errorf("error reflect request type: %v", err)
+		return
+	}
+	logger.Debugf("successfully reflect request type")
+	// binding structure according request type
+	switch reqType {
+	case "single_choice":
+		nova.HandleCreateQuestionSingleChoice(c)
+	case "multiple_choice":
+		nova.HandleCreateQuestionMultipleChoice(c)
+	case "judgement":
+		nova.HandleCreateQuestionJudgement(c)
+	case "essay":
+		nova.HandleCreateQuestionEssay(c)
+	default:
+		nova.response400BadRequest(c, errors.New("invalid request type"))
+	}
+	return
+}
 
+func (nova *Nova) HandleCreateQuestionSingleChoice(c *gin.Context) {
+	return
+}
+
+func (nova *Nova) HandleCreateQuestionMultipleChoice(c *gin.Context) {
+	return
+}
+
+func (nova *Nova) HandleCreateQuestionJudgement(c *gin.Context) {
+	return
+}
+
+func (nova *Nova) HandleCreateQuestionEssay(c *gin.Context) {
+	return
 }
 
 func (nova *Nova) HandleDeleteQuestion(c *gin.Context) {
@@ -38,4 +90,72 @@ func (nova *Nova) HandleQueryQuestion(c *gin.Context) {
 
 func (nova *Nova) HandleUpdateQuestion(c *gin.Context) {
 
+}
+
+func (nova *Nova) reflectRequestType(raw []byte) (string, error) {
+	var data map[string]interface{}
+	// unmarshal raw data to data map interface
+	if err := json.Unmarshal(raw, &data); err != nil {
+		return "", err
+	}
+	// check mandatory segment existence
+	if nova.isRequiredFields(data, QuestionSingleChoice{}) {
+		return "single_choice", nil
+	}
+	if nova.isRequiredFields(data, QuestionMultipleChoice{}) {
+		return "multiple_choice", nil
+	}
+	if nova.isRequiredFields(data, QuestionJudgement{}) {
+		return "judgement", nil
+	}
+	if nova.isRequiredFields(data, QuestionEssay{}) {
+		return "essay", nil
+	}
+	return "", errors.New("invalid request")
+}
+
+func (nova *Nova) isRequiredFields(data map[string]interface{}, model interface{}) bool {
+	// reflect type of model
+	t := reflect.TypeOf(model)
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		tag := field.Tag.Get("json")
+		binding := field.Tag.Get("binding")
+		if tag != "" && nova.containsBinding(binding, "required") {
+			if _, exists := data[tag]; !exists {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func (nova *Nova) containsBinding(binding, rule string) bool {
+	if binding == "" {
+		return false
+	}
+	// split binding rules
+	for _, r := range nova.splitBindingRules(binding) {
+		if r == rule {
+			return true
+		}
+	}
+	return false
+}
+
+func (nova *Nova) splitBindingRules(binding string) []string {
+	var rules []string
+	start := 0
+	// check binding char
+	for i, char := range binding {
+		if char == ',' {
+			rules = append(rules, binding[start:i])
+			start = i + 1
+		}
+	}
+	// check binding length
+	if start < len(binding) {
+		rules = append(rules, binding[start:])
+	}
+	return rules
 }
