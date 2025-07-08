b7cfa62dd662db4b227c84355d9abf1866f84a20
Alopex6414 <1029535012@qq.com>
2025-07-08 23:36:02 +0800

    update delete multiple choice questions

--- app/question_test.go
+++ app/question_test.go
@@ -1505,3 +1505,341 @@ func BenchmarkNova_HandleDeleteQuestionSingleChoiceParallel(b *testing.B) {
 		}
 	})
 }
+
+func TestNova_HandleDeleteQuestionMultipleChoice(t *testing.T) {
+	/*---------------------------------------------------------------------------------------
+	// Test Case: TestNova_HandleDeleteQuestionMultipleChoice (multiple-choice)
+	// Test Purpose: Test HandleDeleteQuestionMultipleChoice delete question
+	// Test Steps:
+	// 1. send CreateQuestionId request by using POST method
+	// 2. receive CreateQuestionId response with created questionId by using 201 Created Code
+	// 3. send CreateQuestion request by using POST method
+	// 4. receive CreateQuestion response with created question by using 201 Created Code
+	// 5. send DeleteQuestion request with questionId by using DELETE method
+	// 6. receive DeleteQuestion request by using 204 No Content Code
+	-----------------------------------------------------------------------------------------*/
+	// reset test case
+	_ = resetQuestionTestCase()
+	// start http test service
+	server, router := startQuestionTestService()
+	defer server.Close()
+	/* create questionId */
+	// request content
+	url := server.URL + "/nova/v1/question/Id"
+	// request create questionId
+	wQuestionId := httptest.NewRecorder()
+	reqQuestionId, err := http.NewRequest(http.MethodPost, url, nil)
+	if err != nil {
+		t.Errorf("error creating request: %v", err)
+	}
+	router.ServeHTTP(wQuestionId, reqQuestionId)
+	// return response
+	var reQuestionId string
+	err = json.Unmarshal(wQuestionId.Body.Bytes(), &reQuestionId)
+	if err != nil {
+		t.Errorf("error unmarshal response: %v", err)
+	}
+	// validate response
+	assert.Equal(t, http.StatusCreated, wQuestionId.Code)
+	assert.Equal(t, "application/json", wQuestionId.Header().Get("Content-Type"))
+	assert.NoError(t, uuid.Validate(reQuestionId))
+	/* create question */
+	url = server.URL + "/nova/v1/question/multiple-choice"
+	question := QuestionMultipleChoice{
+		Id:    reQuestionId,
+		Title: "What's the sweetest fruit?",
+		Answers: []QuestionAnswer{
+			QuestionAnswer{
+				"A",
+				"apple",
+			},
+			QuestionAnswer{
+				"B",
+				"watermelon",
+			},
+			QuestionAnswer{
+				"C",
+				"orange",
+			},
+			QuestionAnswer{
+				"D",
+				"peach",
+			},
+		},
+		StandardAnswers: []QuestionAnswer{
+			{
+				"B",
+				"watermelon",
+			},
+			{
+				"D",
+				"peach",
+			},
+		},
+	}
+	body, err := json.Marshal(question)
+	if err != nil {
+		t.Errorf("error marshal question: %v", err)
+	}
+	// request create user
+	wQuestion := httptest.NewRecorder()
+	reqQuestion, err := http.NewRequest(http.MethodPost, url+"/"+reQuestionId, bytes.NewReader(body))
+	if err != nil {
+		t.Errorf("error creating request: %v", err)
+	}
+	reqQuestion.Header.Set("Content-Type", "application/json")
+	router.ServeHTTP(wQuestion, reqQuestion)
+	// return response
+	var resQuestion QuestionMultipleChoice
+	err = json.Unmarshal(wQuestion.Body.Bytes(), &resQuestion)
+	if err != nil {
+		t.Errorf("error unmarshal response: %v", err)
+	}
+	// validate response
+	assert.Equal(t, http.StatusCreated, wQuestion.Code)
+	assert.Equal(t, "application/json", wQuestion.Header().Get("Content-Type"))
+	assert.Equal(t, question.Id, resQuestion.Id)
+	assert.Equal(t, question.Title, resQuestion.Title)
+	assert.Equal(t, question.Answers, resQuestion.Answers)
+	assert.Equal(t, question.StandardAnswers, resQuestion.StandardAnswers)
+	/* delete question */
+	// request content
+	url = server.URL + "/nova/v1/question/multiple-choice"
+	// request delete question
+	wDeleteQuestion := httptest.NewRecorder()
+	reqDeleteQuestion, err := http.NewRequest(http.MethodDelete, url+"/"+reQuestionId+"?type=multiple_choice", nil)
+	if err != nil {
+		t.Errorf("error creating request: %v", err)
+	}
+	router.ServeHTTP(wDeleteQuestion, reqDeleteQuestion)
+	// validate response
+	assert.Equal(t, http.StatusNoContent, wDeleteQuestion.Code)
+}
+
+func BenchmarkNova_HandleDeleteQuestionMultipleChoice(b *testing.B) {
+	/*---------------------------------------------------------------------------------------
+	// Test Case: BenchmarkNova_HandleDeleteQuestionMultipleChoice (multiple-choice)
+	// Test Purpose: Benchmark HandleDeleteQuestionMultipleChoice delete question
+	// Test Steps:
+	// 1. send CreateQuestionId request by using POST method
+	// 2. receive CreateQuestionId response with created questionId by using 201 Created Code
+	// 3. send CreateQuestion request by using POST method
+	// 4. receive CreateQuestion response with created question by using 201 Created Code
+	// 5. send DeleteQuestion request with questionId by using DELETE method
+	// 6. receive DeleteQuestion request by using 204 No Content Code
+	-----------------------------------------------------------------------------------------*/
+	// reset test case
+	_ = resetQuestionTestCase()
+	// start http test service
+	server, router := startQuestionTestService()
+	defer server.Close()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		/* create questionId */
+		// request content
+		url := server.URL + "/nova/v1/question/Id"
+		// request create questionId
+		wQuestionId := httptest.NewRecorder()
+		reqQuestionId, err := http.NewRequest(http.MethodPost, url, nil)
+		if err != nil {
+			b.Errorf("error creating request: %v", err)
+		}
+		router.ServeHTTP(wQuestionId, reqQuestionId)
+		// return response
+		var reQuestionId string
+		err = json.Unmarshal(wQuestionId.Body.Bytes(), &reQuestionId)
+		if err != nil {
+			b.Errorf("error unmarshal response: %v", err)
+		}
+		// validate response
+		assert.Equal(b, http.StatusCreated, wQuestionId.Code)
+		assert.Equal(b, "application/json", wQuestionId.Header().Get("Content-Type"))
+		assert.NoError(b, uuid.Validate(reQuestionId))
+		/* create question */
+		url = server.URL + "/nova/v1/question/multiple-choice"
+		question := QuestionMultipleChoice{
+			Id:    reQuestionId,
+			Title: "What's the sweetest fruit?",
+			Answers: []QuestionAnswer{
+				QuestionAnswer{
+					"A",
+					"apple",
+				},
+				QuestionAnswer{
+					"B",
+					"watermelon",
+				},
+				QuestionAnswer{
+					"C",
+					"orange",
+				},
+				QuestionAnswer{
+					"D",
+					"peach",
+				},
+			},
+			StandardAnswers: []QuestionAnswer{
+				{
+					"B",
+					"watermelon",
+				},
+				{
+					"D",
+					"peach",
+				},
+			},
+		}
+		body, err := json.Marshal(question)
+		if err != nil {
+			b.Errorf("error marshal question: %v", err)
+		}
+		// request create user
+		wQuestion := httptest.NewRecorder()
+		reqQuestion, err := http.NewRequest(http.MethodPost, url+"/"+reQuestionId, bytes.NewReader(body))
+		if err != nil {
+			b.Errorf("error creating request: %v", err)
+		}
+		reqQuestion.Header.Set("Content-Type", "application/json")
+		router.ServeHTTP(wQuestion, reqQuestion)
+		// return response
+		var resQuestion QuestionMultipleChoice
+		err = json.Unmarshal(wQuestion.Body.Bytes(), &resQuestion)
+		if err != nil {
+			b.Errorf("error unmarshal response: %v", err)
+		}
+		// validate response
+		assert.Equal(b, http.StatusCreated, wQuestion.Code)
+		assert.Equal(b, "application/json", wQuestion.Header().Get("Content-Type"))
+		assert.Equal(b, question.Id, resQuestion.Id)
+		assert.Equal(b, question.Title, resQuestion.Title)
+		assert.Equal(b, question.Answers, resQuestion.Answers)
+		assert.Equal(b, question.StandardAnswers, resQuestion.StandardAnswers)
+		/* delete question */
+		// request content
+		url = server.URL + "/nova/v1/question/multiple-choice"
+		// request delete question
+		wDeleteQuestion := httptest.NewRecorder()
+		reqDeleteQuestion, err := http.NewRequest(http.MethodDelete, url+"/"+reQuestionId+"?type=multiple_choice", nil)
+		if err != nil {
+			b.Errorf("error creating request: %v", err)
+		}
+		router.ServeHTTP(wDeleteQuestion, reqDeleteQuestion)
+		// validate response
+		assert.Equal(b, http.StatusNoContent, wDeleteQuestion.Code)
+	}
+}
+
+func BenchmarkNova_HandleDeleteQuestionMultipleChoiceParallel(b *testing.B) {
+	/*---------------------------------------------------------------------------------------
+	// Test Case: BenchmarkNova_HandleDeleteQuestionMultipleChoice (multiple-choice)
+	// Test Purpose: Benchmark HandleDeleteQuestionMultipleChoice delete question
+	// Test Steps:
+	// 1. send CreateQuestionId request by using POST method
+	// 2. receive CreateQuestionId response with created questionId by using 201 Created Code
+	// 3. send CreateQuestion request by using POST method
+	// 4. receive CreateQuestion response with created question by using 201 Created Code
+	// 5. send DeleteQuestion request with questionId by using DELETE method
+	// 6. receive DeleteQuestion request by using 204 No Content Code
+	-----------------------------------------------------------------------------------------*/
+	// reset test case
+	_ = resetQuestionTestCase()
+	// start http test service
+	server, router := startQuestionTestService()
+	defer server.Close()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			/* create questionId */
+			// request content
+			url := server.URL + "/nova/v1/question/Id"
+			// request create questionId
+			wQuestionId := httptest.NewRecorder()
+			reqQuestionId, err := http.NewRequest(http.MethodPost, url, nil)
+			if err != nil {
+				b.Errorf("error creating request: %v", err)
+			}
+			router.ServeHTTP(wQuestionId, reqQuestionId)
+			// return response
+			var reQuestionId string
+			err = json.Unmarshal(wQuestionId.Body.Bytes(), &reQuestionId)
+			if err != nil {
+				b.Errorf("error unmarshal response: %v", err)
+			}
+			// validate response
+			assert.Equal(b, http.StatusCreated, wQuestionId.Code)
+			assert.Equal(b, "application/json", wQuestionId.Header().Get("Content-Type"))
+			assert.NoError(b, uuid.Validate(reQuestionId))
+			/* create question */
+			url = server.URL + "/nova/v1/question/multiple-choice"
+			question := QuestionMultipleChoice{
+				Id:    reQuestionId,
+				Title: "What's the sweetest fruit?",
+				Answers: []QuestionAnswer{
+					QuestionAnswer{
+						"A",
+						"apple",
+					},
+					QuestionAnswer{
+						"B",
+						"watermelon",
+					},
+					QuestionAnswer{
+						"C",
+						"orange",
+					},
+					QuestionAnswer{
+						"D",
+						"peach",
+					},
+				},
+				StandardAnswers: []QuestionAnswer{
+					{
+						"B",
+						"watermelon",
+					},
+					{
+						"D",
+						"peach",
+					},
+				},
+			}
+			body, err := json.Marshal(question)
+			if err != nil {
+				b.Errorf("error marshal question: %v", err)
+			}
+			// request create user
+			wQuestion := httptest.NewRecorder()
+			reqQuestion, err := http.NewRequest(http.MethodPost, url+"/"+reQuestionId, bytes.NewReader(body))
+			if err != nil {
+				b.Errorf("error creating request: %v", err)
+			}
+			reqQuestion.Header.Set("Content-Type", "application/json")
+			router.ServeHTTP(wQuestion, reqQuestion)
+			// return response
+			var resQuestion QuestionMultipleChoice
+			err = json.Unmarshal(wQuestion.Body.Bytes(), &resQuestion)
+			if err != nil {
+				b.Errorf("error unmarshal response: %v", err)
+			}
+			// validate response
+			assert.Equal(b, http.StatusCreated, wQuestion.Code)
+			assert.Equal(b, "application/json", wQuestion.Header().Get("Content-Type"))
+			assert.Equal(b, question.Id, resQuestion.Id)
+			assert.Equal(b, question.Title, resQuestion.Title)
+			assert.Equal(b, question.Answers, resQuestion.Answers)
+			assert.Equal(b, question.StandardAnswers, resQuestion.StandardAnswers)
+			/* delete question */
+			// request content
+			url = server.URL + "/nova/v1/question/multiple-choice"
+			// request delete question
+			wDeleteQuestion := httptest.NewRecorder()
+			reqDeleteQuestion, err := http.NewRequest(http.MethodDelete, url+"/"+reQuestionId+"?type=multiple_choice", nil)
+			if err != nil {
+				b.Errorf("error creating request: %v", err)
+			}
+			router.ServeHTTP(wDeleteQuestion, reqDeleteQuestion)
+			// validate response
+			assert.Equal(b, http.StatusNoContent, wDeleteQuestion.Code)
+		}
+	})
+}
